15ed76c851001c5a8299acaf82fa8037159ad956
Kuldeep Kumar Rana <142797995+kuldeepkumarrana@users.noreply.github.com>
2024-05-04 13:01:15 +0530

    Add files via upload

---
+++ TheDotStudio/script.js
@@ -0,0 +1,6 @@
+// function show(){
+//     document.getElementById("details").style.transform="translate3d(0, 0, 0)";
+// }
+// function hide(){
+//     document.getElementById("details").style.transform="translate3d(-500px, 500px, -500px)";
+// }
